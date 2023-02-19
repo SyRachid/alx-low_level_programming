b358d0fb95deb72bb2deb3e86bc9ef37cd6605b5
SyRachid <syrachidsanogo78@gmail.com>
2023-02-19 01:56:21 -0800

    new commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,9 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- * main documentation
- * input n
- * return:ALWAYS
+ * main - Entry point
+ *
+ * Return:Always 0 (Success)
  */
 
 int main(void)
