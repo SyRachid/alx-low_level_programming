f619d7412b0c12731106d5097e0d56ca233a4342
SyRachid <syrachidsanogo78@gmail.com>
2023-02-21 07:20:20 -0800

    new commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main -Entry point
  *
@@ -7,6 +7,13 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	int i;
+	char tab[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(tab[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+int _putchar(char c);
