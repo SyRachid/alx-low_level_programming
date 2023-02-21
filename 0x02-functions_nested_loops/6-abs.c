1f2630d8f14edb412209238b21dd62ee64bb4e2a
SyRachid <syrachidsanogo78@gmail.com>
2023-02-21 14:25:19 -0800

    new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - function to show the absolute value of integer
+ *
+ * @c: input of the function , the type is integer
+ *
+ * Return: the absolute value of input
+ */
+
+int _abs(int c)
+{
+	return (abs(c));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,11 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int c);
+int _abs(int c);
