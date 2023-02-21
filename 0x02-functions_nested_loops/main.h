56af75cf621476f492796cc7bffe5a2b1ebb26a3
SyRachid <syrachidsanogo78@gmail.com>
2023-02-21 10:12:06 -0800

    new commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
