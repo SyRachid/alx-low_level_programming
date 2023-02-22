48ce6e59e0dcdde105df5deb672f478c755ecd2a
SyRachid <syrachidsanogo78@gmail.com>
2023-02-22 03:24:29 -0800

    new commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,27 +14,11 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
-			if (hour < 10)
-			{
-				_putchar('0');
-				_putchar(hour / 10);
-			}
-			else
-			{
-				_putchar((hour / 10) + '0');
-				_putchar((hour % 10) + '0');
-			}
+			_putchar((hour / 10)  + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			if (minute < 10)
-			{
-				_putchar('0');
-				_putchar(minute + '0');
-			}
-			else
-			{
-				_putchar((minute / 10) + '0');
-				_putchar((minute % 10) + '0');
-			}
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
 	}
