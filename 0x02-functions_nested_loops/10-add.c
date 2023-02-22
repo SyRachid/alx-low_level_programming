6f95d3d13af5cd9a5792b831390547bec7b12a4a
SyRachid <syrachidsanogo78@gmail.com>
2023-02-22 10:38:43 -0800

    new commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function to add two integers
+ *
+ * @n:thirst input
+ * @m:second input
+ *
+ * Return: the sum of two integers
+ */
+
+int add(int n, int m)
+{
+	return ((n + m));
+}
