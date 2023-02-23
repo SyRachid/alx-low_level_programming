924c4f996c55691f47d9a79860da5ed31eda7621
SyRachid <syrachidsanogo78@gmail.com>
2023-02-23 11:43:30 -0800

    new commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - function to print the  result of the product of integers
+ *
+ * @a:first input
+ *
+ * @b:second input
+ *
+ * Return: the result of product of a by c
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
