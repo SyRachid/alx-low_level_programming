a906f1c90971d957f053ec6889a620f79acfb8e3
SyRachid <syrachidsanogo78@gmail.com>
2023-02-23 13:02:41 -0800

    new commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_diagonal - function to print in the terminal a diagnonal
+ *
+ * @n:input which define the number of "\\" character
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < (i + 1); j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		 }
+	}
+}
