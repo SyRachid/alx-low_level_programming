4d28bce977dde5bcc6a7e98631b3951a3c6cbab9
SyRachid <syrachidsanogo78@gmail.com>
2023-02-27 11:11:18 -0800

    new commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - function to print string in reverse
+ *
+ * @s: pointer on string
+ *
+ * return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	int i;
+
+	for (i = len; i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
