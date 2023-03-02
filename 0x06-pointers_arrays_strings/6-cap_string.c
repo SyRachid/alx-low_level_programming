7d6d6dd93a11de73ea3b7fccaa0b5c864cb0c523
SyRachid <syrachidsanogo78@gmail.com>
2023-03-02 15:02:20 -0800

    new commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string -function to capitalizes all words of a string
+ *
+ * @str: the input of function
+ *
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	char *p = str;
+	int cap_next = 1;
+
+	while (*p)
+	{
+		if (cap_next && isalpha(*p))
+	{
+			*p = toupper(*p);
+			cap_next = 0;
+		}
+		else if (strchr(" \t\n,;.!?\"(){}", *p))
+		{
+			cap_next = 1;
+		}
+		p++;
+	}
+	return (str);
+}
