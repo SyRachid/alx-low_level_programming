b8e0a0a55ce16bbd91b1b4f5385e803fbd22b66e
SyRachid <syrachidsanogo78@gmail.com>
2023-03-07 10:38:27 -0800

    new commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * set_string- function tthats sets the value of pointer to a char
+ *
+ * @s: pointer to pointer
+ *
+ * @to: pointer to receiv the value
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
