9b04b8fc6ab55ce7c8eaa4811836fa86e711b7ca
root <syrachidsanogo78@gmail.com>
2023-05-24 18:17:55 -0700

    new commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 -print all digit from n to 98
+ *
+ * @n: limit of the numbers
+ *
+ * Return:Always 0
+ */
+
+void print_to_98(int n)
+{
+	int i = 0;
+	if(n < 98)
+	{
+		for(i = n; i <= 98; i++)
+		{
+				printf("%d",i);
+				if(i < 98)
+				{
+					printf(",");
+					printf(" ");
+				}
+				if(i == 98)
+				{
+					printf("\n");
+				}
+			}
+		}
+		if(n > 98)
+		{
+			for(i = n; i > 98; i--)
+			{
+				printf("%d",i);
+				if(i > 98)
+				{
+					printf(",");
+					printf(" ");
+				}
+				if(i == 98)
+				{
+					printf("\n");
+				}
+			}
+		}
+}
