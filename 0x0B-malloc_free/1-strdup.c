0b626b3f9efe18f9a5aa457d744d27f717fe41e4
root <syrachidsanogo78@gmail.com>
2023-06-13 14:15:54 -0700

    new commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,24 +2,26 @@
 /**
  * _strdup- function wich return a pointer to the new value of strin
  *
- * @str -pointer of string
+ * @str-pointer of string
  *
  * Return: pointer to string
  */
 char *_strdup(char *str)
 {
 	char *ptr;
-	int length = strlen(str);
+	int length;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+	length = strlen(str);
 	ptr = (char *) malloc(sizeof(char) * (length + 1));
 	if (ptr == NULL)
 	{
+		free(ptr);
 		return (NULL);
 	}
 	strcpy(ptr, str);
-	return (str);
+	return (ptr);
 }
