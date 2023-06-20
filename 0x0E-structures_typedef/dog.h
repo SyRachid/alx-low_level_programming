632c8f24b919f9038d4e063f2c5d88533ac55e0e
root <syrachidsanogo78@gmail.com>
2023-06-20 14:58:10 -0700

    new commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog-struct for dog
+ *
+ * @name:variable
+ *
+ * @owner:variable
+ *
+ * @age:variable
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
