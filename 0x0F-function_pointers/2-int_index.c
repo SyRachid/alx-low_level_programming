da05d48379558b9b01f802553a02a7328f549ca9
root <syrachidsanogo78@gmail.com>
2023-06-22 13:02:52 -0700

    new commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,36 @@
+#include "function_pointers.h"
+/**
+ * int_index- function to find an index of integer
+ *
+ * @array: array of integers
+ *
+ * @size: size of array
+ *
+ * @cmp: function to compare value
+ *
+ * Return: integer;
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int integer = -1;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return(integer);
+	}
+	if (size <= 0)
+	{
+		integer = 0;
+		return (integer);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			integer = i;
+			break;
+		}
+	}
+	return (integer);
+}
