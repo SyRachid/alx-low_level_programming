b2700cb466c7b878dac156642ec221ab9471f6fb
root <syrachidsanogo78@gmail.com>
2023-07-03 02:22:20 -0700

    new commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,13 +6,13 @@
  *
  * Description: singly linked list node structure
  */
-#include <stddef.h>
 typedef struct list_s
 {
     char *str;
     unsigned int len;
     struct list_s *next;
 } list_t;
-
+#include <stddef.h>
 int _putchar(char c);
 size_t print_list(const list_t *h);
+
