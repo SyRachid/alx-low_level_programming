6143c031b1ad5d22df3282856274854bdafb0188
root <syrachidsanogo78@gmail.com>
2023-07-05 10:03:18 -0700

    new commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,41 +1,40 @@
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
-* insert_nodeint_at_index - Insert a new node at a given positiion.
-* @head: First node address.
-* @idx: Position of the new node to be inserted in.
-* @n: Data of the new node.
-* Return: Address of the new node.
-**/
+ * insert_nodeint_at_index-function that inserts a new node at given position
+ * @head: the first node
+ *
+ * @idx: index where new node must to be add
+ *
+ * @n:the value of new node
+ *
+ * Return: the address of the new node
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *copy = *head;
-	unsigned int node;
+	listint_t *newnode = malloc(sizeof(listint_t));
+	listint_t *current;
+	listint_t *memory;
+	unsigned int i = 0;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (*head == NULL && idx == 0)
+	{
+		newnode->next = NULL;
+		newnode->n = n;
+		*head = newnode;
+		return (newnode);
+	}
+	current = *head;
+	while (i < idx && current != NULL)
+	{
+		memory = current;
+		current = current->next;
+	}
+	if (current == NULL)
 		return (NULL);
-
-	new->n = n;
-
-	if (idx == 0)
-	{
-		new->next = copy;
-		*head = new;
-		return (new);
-	}
-
-	for (node = 0; node < (idx - 1); node++)
-	{
-		if (copy == NULL || copy->next == NULL)
-			return (NULL);
-
-		copy = copy->next;
-	}
-
-	new->next = copy->next;
-	copy->next = new;
-
-	return (new);
+	newnode->n = n;
+	newnode->next = current;
+	memory->next = newnode;
+	return (newnode);
 }
