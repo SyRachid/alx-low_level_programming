361e64a373f938a744956cf2b5d71ce70a62a2c5
root <syrachidsanogo78@gmail.com>
2023-07-05 08:09:30 -0700

    new commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,10 +16,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	while (i < index)
+	while (i < index && head != NULL)
 	{
 		head = head->next;
 		i++;
 	}
-	return (head);
+	if (head == NULL)
+		return (NULL);
+	else
+		return (head);
 }
