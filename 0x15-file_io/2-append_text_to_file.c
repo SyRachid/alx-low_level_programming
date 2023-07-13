b73d2acd2bc2f86e147c982d7fa2b36a0f12da01
root <syrachidsanogo78@gmail.com>
2023-07-13 13:05:40 -0700

    new commit for append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,46 +1,38 @@
+#include <unistd.h>
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
 /**
-* append_text_to_file - Append text at the end of a file
-* @filename: name of the file to create.
-* @text_content: text to write inside the file.
-*
-* Return: 1 if text_content was appened, -1 otherwise.
-*/
+ * append_text_to_file -function that appends text at the end of a file
+ *
+ * @filename:the name of file
+ *
+ * @text_content: the text to add
+ *
+ * Return: 1 or -1;
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd_open, fd_write;
-if (filename == NULL)
-{
-return (-1);
-}
-fd_open = open(filename, O_RDWR | O_APPEND);
-if (fd_open == -1)
-{
-return (-1);
-}
-if (text_content != NULL)
-{
-fd_write = write(fd_open, text_content, _strlen(text_content));
-if (fd_write == -1)
-{
-return (-1);
-}
-}
-close(fd_open);
-return (1);
-}
+	FILE *file;
+	int written;
 
-/**
-* _strlen - calculate the lenght of a string.
-* @str: array of characters.
-*
-* Return: lenght of the string.
-*/
-int _strlen(char *str)
-{
-	int count = 0;
-
-	while (*str++)
-		count++;
-	return (count);
+	if (filename == NULL)
+		return (-1);
+	file = fopen(filename, "a");
+	if (file == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fclose(file);
+		return (-1);
+	}
+	text_content[strlen(text_content)+1] = '\0';
+	written = fprintf(file, "%s", text_content);
+	if (written < 0)
+	{
+		fclose(file);
+		return (-1);
+	}
+	fclose(file);
+	return (1);
 }
