9f7254f6cc9d345c37066265a31dc987afa57d70
root <syrachidsanogo78@gmail.com>
2023-07-13 04:41:54 -0700

    new commit create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,46 +1,37 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 /**
-* create_file - create a file.
-* @filename: name of the file to create.
-* @text_content: text to write inside the file.
-*
-* Return: 1 if the file was created, -1 otherwise.
-*/
+ * create_file -function that creates file
+ *
+ * @filename: the name of file to create
+ *
+ * @text_content: the text to fill
+ *
+ * Return: 1 or -1
+ */
 int create_file(const char *filename, char *text_content)
 {
-int fd_open, fd_write;
-if (filename == NULL)
-{
-return (-1);
-}
-fd_open = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-if (fd_open == -1)
-{
-return (-1);
-}
-if (text_content != NULL)
-{
-fd_write = write(fd_open, text_content, _strlen(text_content));
-if (fd_write == -1)
-{
-return (-1);
-}
-}
-close(fd_open);
-return (1);
-}
+	FILE *file;
+	int write;
 
-/**
-* _strlen - calculate the lenght of a string.
-* @str: array of characters.
-* Return: lenght of the string.
-*/
-int _strlen(char *str)
-{
-int count = 0;
-while (*str++)
-{
-count++;
-}
-return (count);
+	if (filename == NULL)
+		return (-1);
+	file = fopen(filename, "w");
+	if (file == NULL)
+		return (-1);
+	chmod(filename, S_IRUSR | S_IWUSR);
+	if (text_content != NULL)
+	{
+		write = fputs(text_content, file);
+		if (write == EOF)
+		{
+		fclose(file);
+		return (-1);
+		}
+	}
+	fclose(file);
+	return (1);
 }
